aeefc3ad7988d733cdd6cadc3cc6a9932dc547fc
Kyparn <93533136+Kyparn@users.noreply.github.com>
2022-06-01 00:37:36 +0200

    app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Sidebar from './components/Sidebar'
+
+import { Provider } from 'react-redux'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+
+import user from 'reducers/user'
+
+const reducer = combineReducers({
+  user: user.reducer,
+})
+
+const store = configureStore({ reducer })
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <Provider store={store}>
+      <Header />
+      <Sidebar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </Provider>
   )
 }

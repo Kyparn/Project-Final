fcef8fcb93df7ca70975797b02fece474cd6fe34
Kyparn <93533136+Kyparn@users.noreply.github.com>
2022-06-21 10:53:46 +0200

    backend

--- frontend/src/components/utils/API.js
+++ frontend/src/components/utils/API.js
@@ -1,4 +1,4 @@
-export const API = `https://final-project-simon.herokuapp.com/`
+export const API = `https://final-project-simon.herokuapp.com`
 
 export const API_URL = (slug) => `${API}/${slug}`
 

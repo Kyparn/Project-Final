b9705124715c4085f10a38e6e31d40fffbb4283e
Kyparn <93533136+Kyparn@users.noreply.github.com>
2022-06-21 11:16:11 +0200

    login

--- frontend/src/components/utils/API.js
+++ frontend/src/components/utils/API.js
@@ -6,7 +6,3 @@ export const BLOGG_URL = `https://final-project-simon.herokuapp.com/blogg`
 
 export const LIKES_URL = (id) =>
   `https://final-project-simon.herokuapp.com/blogg/${id}/like`
-
-// const BASE_URL = "https://j-s-api.herokuapp.com";
-
-// export const API_URL = (slug) => `${BASE_URL}/${slug}`;
